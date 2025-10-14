8040983719a121db354732af80289b9ba73e7db4
Mahdee Hasan <mahdeeh45@gmail.com>
2025-10-14 10:06:03 +0600

    adding conversationdrawer

--- frontend/src/features/chat/components/ConversationDrawer.jsx
+++ frontend/src/features/chat/components/ConversationDrawer.jsx
@@ -1,58 +1,25 @@
-import { Minimize, SendHorizonal, User, X } from "lucide-react";
-import React, { useState } from "react";
-import { FaUser } from "react-icons/fa";
-import { IoAttach } from "react-icons/io5";
+import { Minimize, User, X } from "lucide-react";
+import React from "react";
+
+import MessageBody from "./MessageBody";
 
 const ConversationDrawer = () => {
-  const [message, setMessage] = useState([0, 1, 2, 3, 4, 5, 6]);
-  const addMessage = () => {
-    const i = message.length;
-    setMessage((prev) => [...prev, i]);
-  };
   return (
-    <div className="w-80 z-50 bg-white rounded-2xl right-5 h-95 ring fixed bottom-1 flex flex-col ">
-      <div className="flex rounded-t-2xl gap-2 h-15 justify-between px-3 bg-gray-500 items-center w-full">
+    <div className="w-85 z-50 bg-white rounded-xl right-5 h-100 ring-2 flex fixed bottom-3 flex-col ">
+      <div className="flex rounded-t-xl gap-2 h-12 justify-between px-3 bg-gray-500 items-center w-full">
         <div className="flex items-center gap-2">
           {" "}
           <User className="w-8 h-8  bg-gray-400 text-gray-200 rounded-full" />
           <p className="h-5 w-30 bg-gray-400 animate-pulse"></p>
         </div>
         <div className="flex items-center gap-3 text-gray-400">
-          <Minimize />
-          <X />
+          <Minimize className="cursor-pointer" />
+          <X className="cursor-pointer" />
         </div>
       </div>
-      <div className="flex flex-col overflow-y-scroll gap-2">
-        {message.map((i, _, arr) => (
-          <div
-            key={i}
-            className={`flex items-center gap-2 mx-1 animate-pulse ${
-              i % 2 === 0 ? "justify-start" : "justify-end"
-            }`}
-          >
-            {i % 2 === 0 && (
-              <FaUser className="rounded-full ring text-2xl drop-down" />
-            )}
-            <div
-              className={`w-8/12 h-8 mb-2 rounded-tl-lg rounded-br-lg bg-gray-600 drop-down`}
-              style={{
-                animationDelay: `${(arr.length - 1 - i) * 0.1}s`, // reverse delay
-              }}
-            ></div>
-            {i % 2 !== 0 && (
-              <FaUser className="rounded-full ring text-gray-400 drop-down text-2xl" />
-            )}
-          </div>
-        ))}
-      </div>
-      <div className="h-1/10 rounded-2xl p-5 justify-between bg-gray-400 flex items-center ">
-        <IoAttach className="text-2xl" />
-        <input
-          type="text"
-          autoFocus
-          className="rounded-lg w-4/5 h-7 bg-gray-100 p-1"
-        />
-        <SendHorizonal onClick={addMessage} />
+      <div className="h-87">
+        {" "}
+        <MessageBody />
       </div>
     </div>
   );
---
+++ frontend/src/features/chat/components/MessageBody.jsx
@@ -0,0 +1,61 @@
+import { SendHorizonal } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { IoAttach } from "react-icons/io5";
+
+const MessageBody = () => {
+  const [message, setMessage] = useState([0, 1, 2, 3, 4, 5, 6]);
+  const bottomRef = useRef(null);
+
+  const addMessage = () => {
+    setMessage((prev) => [...prev, prev.length]);
+  };
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
+  return (
+    <>
+      <div className="h-9/10 max-h-9/10 flex flex-col justify-end">
+        <div className="flex flex-col scrollbar-hide overflow-y-scroll gap-2">
+          {message.map((i, _, arr) => (
+            <div
+              key={i}
+              className={`flex items-center gap-2 mx-1 animate-pulse ${
+                i % 2 === 0 ? "justify-start" : "justify-end"
+              }`}
+            >
+              {i % 2 === 0 && (
+                <FaUser className="rounded-full ring text-2xl drop-down" />
+              )}
+              <div
+                className="w-8/12 h-8 mb-2 rounded-tl-lg rounded-br-lg bg-gray-600 drop-down"
+                style={{
+                  animationDelay: `${(arr.length - 1 - i) * 0.1}s`, // reverse delay
+                }}
+              ></div>
+              {i % 2 !== 0 && (
+                <FaUser className="rounded-full ring text-gray-400 drop-down text-2xl" />
+              )}
+            </div>
+          ))}
+
+          <div ref={bottomRef} />
+        </div>
+      </div>
+
+      <div className="h-1/10 p-5 justify-between rounded-b-xl bg-gray-400 flex items-center">
+        <IoAttach className="text-2xl" />
+        <input
+          type="text"
+          autoFocus
+          className="rounded-lg w-4/5 h-7 bg-gray-100 p-1"
+        />
+        <SendHorizonal onClick={addMessage} className="cursor-pointer" />
+      </div>
+    </>
+  );
+};
+
+export default MessageBody;
